a5a2ba6e3af573cf4525d4d1f19dc377f064ccf9
p1k0chu <p1k0chu.vim@gmail.com>
2026-02-28 01:15:55 +0200

    Fix execvp_safe
    argv must end in NULL, also stringify the error

--- src/unix.rs
+++ src/unix.rs
@@ -1,23 +1,28 @@
 use crate::error::Error;
 use std::ffi::{CStr, c_char, c_int};
-use std::iter;
+use std::{iter, ptr};
 
 unsafe extern "C" {
     fn execvp(path: *const c_char, argv: *const *const c_char) -> i32;
     fn __errno_location() -> *mut c_int;
     pub safe fn geteuid() -> u32;
     pub safe fn getegid() -> u32;
+    fn strerror(errno: c_int) -> *const c_char;
 }
 
 pub fn execvp_safe(path: &CStr, argv: &[&CStr]) -> Error {
-    let argv: Vec<_> = iter::once(path)
-        .chain(argv.to_owned())
-        .map(|i| i.as_ptr())
+    let argv: Vec<_> = iter::once(&path)
+        .chain(argv.iter())
+        .map(|i| (*i).as_ptr())
+        .chain(iter::once(ptr::null()))
         .collect();
 
-    let c = unsafe {
+    let msg = unsafe {
         execvp(path.as_ptr(), argv.as_ptr());
-        *__errno_location()
+        let errno = *__errno_location();
+        let msg = strerror(errno);
+        CStr::from_ptr(msg)
     };
-    format!("execvp failed: {}", c).into()
+
+    format!("execvp failed: {}", msg.to_string_lossy()).into()
 }
